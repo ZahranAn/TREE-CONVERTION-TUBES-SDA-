a59613d00bb292a08f684991e1d5894f624ad70e
rahzlavnia <sarah.ajjh69@gmail.com>
2023-04-16 19:57:24 +0700

    add header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+/*
+File        : main.cpp
+Deskripsi   : driver / main program untuk program convert non binary tree ke binary tree (AVL)
+Anggota     : Bhisma Chandra Yudha      221524037
+              Sarah                     221524059  
+              Zahran Augerah Rizqullah  221524063
+Kelas/Prodi : 1B/D4 Teknik Informatika
+*/
+
 #include "header.h"
+#include <stdio.h>
+#include "body.cpp"
 
 int main(){
 	
