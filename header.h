4cf4b3ec073ef8d555f726847e3929b7beab29a3
rahzlavnia <sarah.ajjh69@gmail.com>
2023-04-16 19:47:01 +0700

    edit header

--- header.h
+++ header.h
@@ -1,9 +1,18 @@
+/*
+File        : header.h
+Deskripsi   : Deklarasi modul dan kamus data program convert non binary tree ke binary tree (AVL)
+Anggota     : Bhisma Chandra Yudha      221524037
+              Sarah                     221524059  
+              Zahran Augerah Rizqullah  221524063
+Kelas/Prodi : 1B/D4 Teknik Informatika
+*/
+
 #ifndef header_H
 #define header_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+#include "boolean.h"
 
 
 
