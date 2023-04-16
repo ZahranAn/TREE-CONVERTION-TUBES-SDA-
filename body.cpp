861b905237534a08c5894abf3679811bef59ab32
rahzlavnia <sarah.ajjh69@gmail.com>
2023-04-16 19:55:37 +0700

    add header in all file

--- body.cpp
+++ body.cpp
@@ -1,2 +1,13 @@
+/*
+File        : body.cpp
+Deskripsi   : body prototype program convert non binary tree ke binary tree (AVL)
+Anggota     : Bhisma Chandra Yudha      221524037
+              Sarah                     221524059  
+              Zahran Augerah Rizqullah  221524063
+Kelas/Prodi : 1B/D4 Teknik Informatika
+*/
+
 #include "header.h"
+#include <malloc.h>
+
 
--- boolean.h
+++ boolean.h
@@ -1,3 +1,12 @@
+/*
+File        : boolean.h
+Deskripsi   : header file untuk menangani type boolean
+Anggota     : Bhisma Chandra Yudha      221524037
+              Sarah                     221524059  
+              Zahran Augerah Rizqullah  221524063
+Kelas/Prodi : 1B/D4 Teknik Informatika
+*/
+
 #ifndef boolean_H
 #define boolean_H
 #define true 1
