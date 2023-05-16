b49ba5e28641cb066bbba5e7baafdab91a506584
rahzlavnia <sarah.ajjh69@gmail.com>
2023-05-16 22:58:49 +0700

    add avl print tree, separate search node

--- main.cpp
+++ main.cpp
@@ -16,20 +16,7 @@ int main()
 	NBTree nb = NULL;
 	BTree b = NULL;
 	BTree avl = NULL;
-	char filename[20]; //= "FileTree.txt";
-
-	/* INISIALISASI NON-BINARY TREE */
-	// InsertNBnode(&nb, SearchNBnode(nb,'0'),'A');
-	// InsertNBnode(&nb, SearchNBnode(nb,'A'),'B');
-	// InsertNBnode(&nb, SearchNBnode(nb,'A'),'C');
-	// InsertNBnode(&nb, SearchNBnode(nb,'A'),'D');
-	// InsertNBnode(&nb, SearchNBnode(nb,'B'),'E');
-	// InsertNBnode(&nb, SearchNBnode(nb,'C'),'F');
-	// InsertNBnode(&nb, SearchNBnode(nb,'C'),'G');
-	// InsertNBnode(&nb, SearchNBnode(nb,'C'),'H');
-	// InsertNBnode(&nb, SearchNBnode(nb,'D'),'I');
-	// InsertNBnode(&nb, SearchNBnode(nb,'D'),'J');
-	// InsertNBnode(&nb, SearchNBnode(nb,'J'),'K');
+	char filename[20]; 
 
 	/* KONVERSI NON-BINARY TREE */
 	for (;;)
@@ -80,7 +67,7 @@ int main()
 			system("cls");
 			break;
 		case 2:
-			// edit
+			// edit node
 			editTree(&nb);
 			system("pause");
 			system("cls");
@@ -100,65 +87,25 @@ int main()
 		case 5:
 			// convert & print
 			ConvertNBtree(nb, &b, &avl);
-			c:
-			printf("Apakah Anda Ingin Mencari Node (Level/Depth Nya Juga)?(Y/N)\n");
-			char pilihan;
-			scanf(" %c", &pilihan);
-			if (pilihan == 'Y' || pilihan == 'y')
-			{
-				printf("Masukkan Node Yang Ingin Anda Cari: ");
-				char nama;
-				scanf(" %c", &nama);
-				Baddr P;
-				P = SearchBnode(b, nama);
-				if (P == NULL)
-				{
-					printf("Node Tidak Ditemukan!\n");
-				}
-				else
-				{
-					int kedalaman = depth(b);
-					printf("Kedalaman Binary Tree %d\n", kedalaman);
-					int level = findLevel(b, nama, 1);
-					printf("Level Dari Node %c Binary Tree Adalah %d\n", nama, level);
-					kedalaman = depth(avl);
-					printf("Kedalaman AVL Tree %d\n", kedalaman);
-					level = findLevel(avl, nama, 1);
-					printf("Level Dari Node %c AVL Tree Adalah %d\n", nama, level);
-				}
-				ViewTraversal(nb, b, avl);
-			}
-			else if (pilihan == 'N' || pilihan == 'n')
-			{
-				ViewTraversal(nb, b, avl);
-			}
-			else
-			{
-				printf("Masukkan input yang sesuai!\n");
-				goto c;
-			}
+			ViewTraversal(nb, b, avl);
+			printf("\n\nAVL TREE\n");
+			printAVLTree(avl);
 			system("pause");
 			system("cls");
 			break;
 		case 6:
 			// save tree to file
-			if (nb == NULL)
-			{
-				printf("Tree Masih Kosong!\n");
-				system("pause");
-				system("cls");
-				break;
-			}
-			else
-			{
-				printf("Silahkan Masukkan Nama File (.txt): ");
-				scanf(" %s", &filename);
-				saveNBTreeToFile(nb, filename);
-			}
+			saveTree(nb);
 			system("pause");
 			system("cls");
 			break;
 		case 7:
+			// search node
+			searchNode(nb, b, avl);
+			system("pause");
+			system("cls");
+			break;	
+		case 8:
 			// exit
 			printf("Terima kasih telah menggunakan program kami!");
 			exit(0);
